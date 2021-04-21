b3c69c3f82b47b8a9578dd8ee9da6d9f9580fc25
givey8 <82672092+givey8@users.noreply.github.com>
2021-04-21 12:05:15 -0500

    Import java.lang.Math
    The import statement is wrong. This is the right one.

--- FoodRunner.java
+++ FoodRunner.java
@@ -1,9 +1,11 @@
 /*Main Runner class*/
+import java.util.Math;
 public class FoodRunner {
         public static void main(String args[]){
             FoodOutline outline=new FoodOutline();
             double amount=outline.play();
             amount=outline.calculateTax(amount);
+            amount=Math.round(amount*100.0)/100.0;
             System.out.println("Your total is " + amount);
             String counts=outline.getOrderCount();
             System.out.println(counts);
